c69c2cf2347b692f5cac971d0007868595549b7c
BFGCUstudent05 <brynn2197@gmail.com>
2026-03-28 11:54:19 -0500

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginRegistrationApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Sex { get; set; }
+
+        public int Age { get; set; }
+
+        public string State { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
